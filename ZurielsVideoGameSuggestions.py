e542ea9b5eb1ab8e5b993d23daa5eddf6ddb86c9
Z <105754053+ZurielBoateng@users.noreply.github.com>
2024-11-26 00:06:25 +0000

    Changed list to dictionary to allow for image key

--- ZurielsVideoGameSuggestions.py
+++ ZurielsVideoGameSuggestions.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from PIL import ImageTk, Image
+from PIL import Image, ImageTk
 import random
 
 
@@ -8,70 +8,83 @@ class ZurielsVideoGameSuggestions:
     def __init__(self, root):
         self.root = root
         self.root.title("Zuriel's Video Game Suggestions")
-        self.root.geometry("500x500")
+        self.root.geometry("500x600")
         self.root.configure(bg="#ECE4f2")
 
         # Title label
-        self.title_label = ttk.Label(root, text="Want to play something fun?", font=("Helvetica", 24, "bold"),
+        self.title_label = ttk.Label(root, text="Want to play something fun?", font=("Helvetica", 25, "bold"),
                                      foreground="#3AC5C7", background="#ECE4f2")
         self.title_label.pack(pady=20)
 
-        # Button to get random videogame
+        # Button to get random video game
         self.get_activity_button = ttk.Button(root, text="Suggest a Video Game", command=self.suggest_random_videogame)
         self.get_activity_button.pack(pady=10)
 
-        # Label to display the videogame
+        # Label to display the video game
         self.activity_label = ttk.Label(root, text="", font=("Helvetica", 14), wraplength=450, justify="center",
                                         foreground="black", background="#ECE4f2")
         self.activity_label.pack(pady=20)
 
+        # Label to display the iamge
+        self.image_label = tk.Label(root, bg="#ECE4f2")
+        self.image_label.pack(pady=20)
+
     def suggest_random_videogame(self):
         # A local list of video games
-        videogames = [
-            "Grand theft auto 5",
-            "Grand Theft Auto: San Andreas",
-            "Grand Theft Auto: Vice City",
-            "Grand Theft Auto IV",
-            "Path of Exile",
-            "Path of Exile II",
-            "Call of Duty: Black Ops III Zombies",
-            "Call of Duty: World at War",
-            "Call of Duty: Black Ops",
-            "Call of Duty: Modern Warfare 2 (2009)",
-            "Call of Duty: Modern Warfare 3 (2011)",
-            " Call of Duty: Black Ops II",
-            "Forza Horizon 5",
-            "Warframe",
-            "Age of Empires II: Definitive Edition",
-            "For Honor",
-            "Assassin's Creed",
-            "Assassin's Creed II",
-            "Assassin's Creed: Brotherhood",
-            "Assassin's Creed: Revelations ",
-            "Assassin's Creed III",
-            "Far Cry 5",
-            "Yakuza 0",
-            "NBA 2K Playgrounds 2",
-            "Desperados III",
-            "Shadow Gambit: The Cursed Crew",
-            "Shadow Tactics: Aiko’s Choice",
-            "Shadow Tactics: Blades of the Shogun",
-            "Dishonored",
-            "Dragon's Dogma: Dark Arisen",
-            "Mafia: Definitive Edition",
-            "Minecraft",
-            "League of Legends - Please dont unironically play this",
-            "Tetris",
-            "Super Smash Bros. Melee",
-            "Super Smash Bros. Ultimate",
-        ]
+        videogames = {
+            "Grand theft auto 5": "images/Gta5.jpg",
+            "Grand Theft Auto: San Andreas": "images/Gtasa.jpg",
+            "Grand Theft Auto: Vice City": "images/Gtavc.jpg",
+            "Grand Theft Auto IV": "images/Gtaiv.jpg",
+            "Path of Exile": "images/poe.jpg",
+            "Path of Exile II": "images/poe2.jpg",
+            "Call of Duty: Black Ops III Zombies": "images/codbo3z.jpg",
+            "Call of Duty: World at War": "images/codwaw.jpg",
+            "Call of Duty: Black Ops": "images/codbo.jpg",
+            "Call of Duty: Modern Warfare 2 (2009)": "images/codmw2.jpg",
+            "Call of Duty: Modern Warfare 3 (2011)": "images/codmw3.jpg",
+            "Call of Duty: Black Ops II": "images/codbo2.jpg",
+            "Forza Horizon 5": "images/fh5.jpg",
+            "Warframe": "images/warframe.jpg",
+            "Age of Empires II: Definitive Edition": "images/aoe2de.jpg",
+            "For Honor": "images/fh.jpg",
+            "Assassin's Creed": "images/ac.jpg",
+            "Assassin's Creed II": "images/ac2.jpg",
+            "Assassin's Creed: Brotherhood": "images/acbh.jpg",
+            "Assassin's Creed: Revelations ": "images/acr.jpg",
+            "Assassin's Creed III": "images/ac3.jpg",
+            "Far Cry 3": "images/fc3.jpg",
+            "Far Cry 5": "images/fc5.jpg",
+            "Yakuza 0": "images/y0.jpg",
+            "NBA 2K Playgrounds 2": "images/nba2kpg2.jpg",
+            "Desperados III": "images/d3.jpg",
+            "Shadow Gambit: The Cursed Crew": "images/sgtcc.jpg",
+            "Shadow Tactics: Aiko’s Choice": "images/stac.jpg",
+            "Shadow Tactics: Blades of the Shogun": "images/stbots.jpg",
+            "Dishonored": "images/dishonored.jpg",
+            "Dragon's Dogma: Dark Arisen": "images/ddda.jpg",
+            "Mafia: Definitive Edition": "images/mde.jpg",
+            "Minecraft": "images/minecraft.jpg",
+            "League of Legends - Please dont unironically play this": "images/lol.jpg",
+            "Tetris": "images/tetris.jpg",
+            "Super Smash Bros. Melee": "images/ssbm.jpg",
+            "Super Smash Bros. Ultimate": "images/ssbu.jpg",
+        }
 
         # Choose a random video game
-        videogame = random.choice(videogames)
+        videogame = random.choice(list(videogames.keys()))
 
         # Display the video game
         self.activity_label.config(text=f"You should try: {videogame}")
 
+        # Load image
+        from PIL import Image
+        image_path = videogames[videogame]
+        image = Image.open(image_path)
+        image = image.resize((300, 370), Image.Resampling.LANCZOS)  # Resize to fit the GUI
+        photo = ImageTk.PhotoImage(image)
+        self.image_label.config(image=photo)
+        self.image_label.image = photo  # Keep a reference to avoid poor collection
 
 # Create the main application window
 root = tk.Tk()
